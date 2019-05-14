c256a16a2793a7d1ff41890ac6a661245f3dce34
Vincent Giersch <vincent@giersch.fr>
2019-05-14 15:10:40 +0200

    feat: add dependency for sandbox

---
+++ README.md
@@ -0,0 +1,4 @@
+# Flat Embed Editor Demo
+
+This is a demo using [our Embed SDK](https://flat.io/developers/embed) with our music notation editor. [You can try it on CodeSandbox here: https://codesandbox.io/s/github/FlatIO/embed-demo-editor-simple](https://codesandbox.io/s/github/FlatIO/embed-demo-editor-simple).
+
--- index.js
+++ index.js
@@ -1,54 +1,58 @@
 // Create embed in the `embed-example` div. By default the embed will fit its container
-var container = document.getElementById('embed-example');
+var container = document.getElementById("embed-example");
+
 var embed = new Flat.Embed(container, {
   // The score hosted on Flat we use here as template.
   // You can also use `embed.loadMusicXML(score)` to load your MsuicXML on the fly:
   // https://flat.io/developers/docs/embed/javascript.html#loadmusicxmlscore-mixed-promisevoid-error
-  score: '56ae21579a127715a02901a6',
+  score: "56ae21579a127715a02901a6",
   // The embed configuration parameters
+  height: "500px",
   embedParams: {
-    mode: 'edit',
-    appId: '59e7684b476cba39490801c2',
+    mode: "edit",
+    appId: "59e7684b476cba39490801c2",
     // Customization: https://flat.io/developers/docs/embed/url-parameters.html
     branding: false,
-    controlsPosition: 'top',
+    controlsPosition: "top"
   }
 });
 
 // Helper function to force downloading the exported file
-var exportFile = function (buffer, mimeType, ext) {
-  var blobUrl = window.URL.createObjectURL(new Blob([buffer], {
-      type: mimeType,
-    }));
-    var a = document.createElement('a');
-    a.href = blobUrl;
-    a.download = 'exported-score.' + ext;
-    document.body.appendChild(a);
-    a.style = 'display: none';
-    a.click();
-    a.remove();
+var exportFile = function(buffer, mimeType, ext) {
+  var blobUrl = window.URL.createObjectURL(
+    new Blob([buffer], {
+      type: mimeType
+    })
+  );
+  var a = document.createElement("a");
+  a.href = blobUrl;
+  a.download = "exported-score." + ext;
+  document.body.appendChild(a);
+  a.style = "display: none";
+  a.click();
+  a.remove();
 };
 
 // Export as MusicXML when the button is clicked
 // https://flat.io/developers/docs/embed/javascript.html#getmusicxmloptions-object-promisestringuint8array-error
-document.getElementById('export-xml').addEventListener('click', function () {
-  embed.getMusicXML({ compressed: true }).then(function (buffer) {
-    exportFile(buffer, 'application/vnd.recordare.musicxml+xml', 'mxl');
+document.getElementById("export-xml").addEventListener("click", function() {
+  embed.getMusicXML({ compressed: true }).then(function(buffer) {
+    exportFile(buffer, "application/vnd.recordare.musicxml+xml", "mxl");
   });
 });
 
 // Export as MIDI when the button is clicked
 // https://flat.io/developers/docs/embed/javascript.html#getmidi-promiseuint8array-error
-document.getElementById('export-midi').addEventListener('click', function () {
-  embed.getMIDI().then(function (buffer) {
-    exportFile(buffer, 'audio/midi', 'mid');
+document.getElementById("export-midi").addEventListener("click", function() {
+  embed.getMIDI().then(function(buffer) {
+    exportFile(buffer, "audio/midi", "mid");
   });
 });
 
 // Export as PNG when the button is clicked
 // https://flat.io/developers/docs/embed/javascript.html#getpngoptions-object-promisestringuint8array-error
-document.getElementById('export-png').addEventListener('click', function () {
-  embed.getPNG().then(function (buffer) {
-    exportFile(buffer, 'image/png', 'png');
+document.getElementById("export-png").addEventListener("click", function() {
+  embed.getPNG().then(function(buffer) {
+    exportFile(buffer, "image/png", "png");
   });
 });
